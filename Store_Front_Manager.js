79febf5254dfdbbaab98a4a21f72ae96196b1a1b
mkkiyoi <mkkiyoi@gmail.com>
2019-11-04 16:28:31 -0800

    Added MySQL node module connection and node moduke import statements to Store_Front_Manager.js. Added function to view all of the products in the database.

---
+++ Store_Front_Manager.js
@@ -0,0 +1,34 @@
+// Node Module Imports
+let inquirer = require('inquirer');
+require('dotenv').config();
+let keys = require('./keys');
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: keys.CREDENTIALS.secret,
+    database: "Store_Front_DB"
+});
+
+let viewAllProducts = () => {
+    connection.query('SELECT ProductID, ProductName, Price FROM `Products`', (error, results, fields) => {
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        console.log('ID     Product                 Price')
+        console.log('------------------------');
+        results.forEach((result) => {
+            console.log(result.ProductID + '      ' + result.ProductName + '                  ' + result.Price);
+            console.log('------------------------')
+        });
+        promptUser();
+    });
+}
