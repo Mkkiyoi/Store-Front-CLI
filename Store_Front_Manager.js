f227708d76d58de1311606d6c71648d4c793cbb6
mkkiyoi <mkkiyoi@gmail.com>
2019-11-04 16:53:20 -0800

    Added functionality to view low inventory and add to inventory in Store_Front_Manager.js

--- Store_Front_Manager.js
+++ Store_Front_Manager.js
@@ -37,8 +37,10 @@ let managerOptions = () => {
                 viewAllProducts();
                 break;
             case 'View Low Inventory':
+                viewLowInventory();
                 break;
             case 'Add to Inventory':
+                addToInventory();
                 break;
             case 'Add New Product':
                 break;
@@ -58,6 +60,42 @@ let viewAllProducts = () => {
             console.log('------------------------')
         });
         connection.end();
+    });
+}
+
+let viewLowInventory = () => {
+    connection.query('SELECT ProductID, ProductName, Price, StockQuantity FROM `Products` WHERE StockQuantity < 5', (error, results, fields) => {
+        // error will be an Error if one occurred during the query
+        // results will contain the results of the query
+        // fields will contain information about the returned results fields (if any)
+        console.log('ID     Product                 Price     Quantity')
+        console.log('------------------------');
+        results.forEach((result) => {
+            console.log(result.ProductID + '      ' + result.ProductName + '                  ' + result.Price + '     ' + result.StockQuantity);
+            console.log('------------------------')
+        });
+        connection.end();
+    });
+}
+
+let addToInventory = () => {
+    inquirer.prompt([
+        {
+            name: 'id',
+            message: 'What is the ID of the product you would like to update?'
+        },
+        {
+            name: 'quantity',
+            message: 'How many are you adding?'
+        }
+    ]).then((response) => {
+        connection.query('SELECT StockQuantity FROM `Products` WHERE ProductID = ' + response.id, (error, results, fields) => {
+            if (error) throw error;
+            let quantityToAdd = result[0].StockQuantity + response.quantity;
+            connection.query('UPDATE Products SET StockQuantity = ? WHERE ProductID = ?', [quantityToAdd, response.id], (error, results, fields) => {
+                if (error) throw error;
+            });
+        });
     });
 }
 
